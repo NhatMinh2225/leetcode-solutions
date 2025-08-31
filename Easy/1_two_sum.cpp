871597862a88cfdea908f4eb27a34ae7e85bdcb9
Nhat Minh <qphun@uic.edu>
2025-08-30 23:18:28 -0500

    Add Two Sum solution

---
+++ Easy/1_two_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> Sum(2);
+        for (int i = 0; i < nums.size()-1; i++){
+            for (int j = i+1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    Sum[0] = i;
+                    Sum[1] = j;
+                }
+            } 
+        }
+        return Sum;
+
+    }
+};
