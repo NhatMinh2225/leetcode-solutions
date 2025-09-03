d053d44468f28d122c94a156a3568468101a3e3d
Nhat Minh <qphun@uic.edu>
2025-09-02 21:07:42 -0500

    Valid Parentheses

---
+++ Easy/20.valid-parentheses.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <int> st;
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == '[' || s[i] == '(' || s[i] == '{'){
+                st.push(s[i]);
+            }
+            if (s[i] == ')'){
+                if(st.top() == '('){
+                    st.pop();
+                }
+                else return false;
+            if (s[i] == '}'){
+                if(st.top() == '{'){
+                    st.pop();
+                }
+                else return false;
+            }
+            if (s[i] == ']'){
+                if(st.top() == '['){
+                    st.pop();
+                }
+                else return false;
+            }
+        }
+    }
+    return true;
+    }
+};
