f33a0dd04e6e578da51db3840b23b77c39f65dfe
Nhat Minh <qphun@uic.edu>
2025-09-16 22:01:14 -0500

    Remove Element

--- Easy/27.remove-element.cpp
+++ Easy/27.remove-element.cpp
@@ -1,15 +1,23 @@
-/*
- * @lc app=leetcode id=27 lang=cpp
- *
- * [27] Remove Element
- */
-
-// @lc code=start
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        
-    }
-};
-// @lc code=end
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
 
+        vector<int> arr(size);
+        if (nums.size() > 0){
+            arr[0] = nums[0];
+        }
+        int count = 1;
+
+        int cur = nums[0]; 
+
+        for (int i = 0; i < size; i++){
+            if (cur != nums[i]){
+                cur = nums[i];
+                arr[count ++] = nums[i];
+            } 
+        }
+
+        for (int i = 0; i < count; i++){
+            nums[i] = arr[i];
+        }
